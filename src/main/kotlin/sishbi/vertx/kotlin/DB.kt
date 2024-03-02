2de9f501a2d9402f3a924e2705561c0247431ef6
Simon Billingsley <simon.billingsley@kodypay.com>
2024-03-02 12:09:55 +0000

    Refactor DB

--- src/main/kotlin/sishbi/vertx/kotlin/DB.kt
+++ src/main/kotlin/sishbi/vertx/kotlin/DB.kt
@@ -5,23 +5,46 @@ import io.vertx.kotlin.pgclient.pgConnectOptionsOf
 import io.vertx.kotlin.sqlclient.poolOptionsOf
 import io.vertx.pgclient.PgBuilder
 import io.vertx.sqlclient.Pool
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import mu.KotlinLogging
+import org.flywaydb.core.Flyway
+
+private val LOG = KotlinLogging.logger {}
 
 class DB(vertx: Vertx) {
     init {
         pool = PgBuilder
             .pool()
             .with(poolOptionsOf(maxSize = 5))
-            .connectingTo(pgConnectOptionsOf(
-                host = configuration.dbHost,
-                port = configuration.dbPort,
-                database = configuration.dbDB,
-                user = configuration.dbUser,
-                password = configuration.dbPassword))
+            .connectingTo(
+                pgConnectOptionsOf(
+                    host = configuration.dbHost,
+                    port = configuration.dbPort,
+                    database = configuration.dbDB,
+                    user = configuration.dbUser,
+                    password = configuration.dbPassword
+                )
+            )
             .using(vertx)
             .build()
     }
 
+    suspend fun initDB() = withContext(Dispatchers.IO) {
+        kotlin.runCatching {
+            Flyway
+                .configure()
+                .dataSource(configuration.dbUrl, configuration.dbUser, configuration.dbPassword)
+                .load()
+                .migrate()
+        }.onFailure {
+            LOG.error(it) { "Failed to migrate DB" }
+            throw it
+        }
+    }
+
     companion object {
-        lateinit var pool : Pool
+        lateinit var pool: Pool
+
     }
 }
--- src/main/kotlin/sishbi/vertx/kotlin/MainVerticle.kt
+++ src/main/kotlin/sishbi/vertx/kotlin/MainVerticle.kt
@@ -4,9 +4,6 @@ import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.CoroutineRouterSupport
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.coAwait
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.flywaydb.core.Flyway
 import kotlin.system.exitProcess
 
 
@@ -14,29 +11,13 @@ private val LOG = mu.KotlinLogging.logger {}
 
 class MainVerticle : CoroutineVerticle(), CoroutineRouterSupport {
     override suspend fun start() {
-        initDB()
-        DB(vertx)
+        DB(vertx).initDB()
         vertx.deployVerticle(HttpVerticle()).coAwait()
         vertx.deployVerticle(GrpcVerticle()).coAwait()
     }
 
     override suspend fun stop() {
         DB.pool.close()
-    }
-
-    private suspend fun initDB() {
-        withContext(Dispatchers.IO) {
-            kotlin.runCatching {
-                Flyway
-                    .configure()
-                    .dataSource(configuration.dbUrl, configuration.dbUser, configuration.dbPassword)
-                    .load()
-                    .migrate()
-            }.onFailure {
-                LOG.error(it) { "Failed to migrate DB" }
-                throw it
-            }
-        }
     }
 }
 
